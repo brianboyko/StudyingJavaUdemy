3041a24df2499ca87dde63babf5768fe787cccb1
Brian Boyko <brian.boyko@gmail.com>
2016-07-04 05:44:17 -0500

    starting to see why I hate java

--- src/com/learning/SimpleClass.java
+++ src/com/learning/SimpleClass.java
@@ -1,11 +1,15 @@
 package com.learning;
 
+import java.util.Random;
+
 public class SimpleClass {
 
-  public int a = 0;
+  public int a = 0; // acess via [instanceVariableName].a
   public int i = 1; 
   public int doublea = 0;
   public int doublei = 0; 
+  public static int r = 10; // access via SimpleClass.r OR [instanceVariableName].r
+  public final int constantInt = 100; // cannot be changed. 
   
   public SimpleClass() {
     // defines the constructor;
@@ -21,6 +25,38 @@ public class SimpleClass {
     this.doublei = d * 2;
   
   }
+  // this is a method, not a function???
+  public void printSomething(){
+    System.out.println("Print Something!");
+  }
+  
+  public void printWord(String word){
+    System.out.println(word);
+  }
+  
+  public int returnSomething(){
+    return 2;
+  }
+  
+  public int calculate(int a, int b){
+    return a * b;
+  }
+  
+  public static String knuthShuffler(String source){
+    String[] sourceArray = source.split("(?!^)");
+    System.out.println(String.join("", sourceArray));
+    String temp;
+    int randInd;
+    Random randGen = new Random(); 
+    for(int i = 0; i < sourceArray.length; i++){
+      temp = sourceArray[i]; 
+      randInd = randGen.nextInt(sourceArray.length);
+      sourceArray[i] = sourceArray[randInd]; 
+      sourceArray[randInd] = temp; 
+    }
+    return String.join("", sourceArray);
+    
+  }
   
   public static void main(String[] args){
     
@@ -30,8 +66,16 @@ public class SimpleClass {
     System.out.println("Runs after constructor");
     System.out.println("ourClass.a: " + ourClass.a + " ourClass.doublea: " + ourClass.doublea);
     System.out.println("ourClass.i: " + ourClass.i + " ourClass.doublei: " + ourClass.doublei);
+    System.out.println("This is static to the class: r = " + SimpleClass.r);
+    ourClass.printSomething();
+    ourClass.printWord("yadda yada");
+    System.out.println(ourClass.returnSomething());
+    System.out.println(ourClass.calculate(4,  5)); 
+    System.out.println("instance shuffler");
+    System.out.println("final " + ourClass.knuthShuffler("alphabet"));
+    System.out.println("class (static) shuffler");
+    System.out.println("final " + SimpleClass.knuthShuffler("alphabet"));
 
-    
   }
   
 }
